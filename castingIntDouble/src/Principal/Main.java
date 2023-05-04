22e69ae1c922f8c403e6f17e11702da2dbc53d6e
Eduardo <eduferr2803@gmail.com>
2023-05-04 13:55:48 -0300

    add initial

---
+++ castingIntDouble/src/Principal/Main.java
@@ -0,0 +1,50 @@
+package Principal;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		/*Calculo do trapézio*/
+		/*
+		double b_menor, B_maior, h, area;
+		
+		b_menor=6.0;
+		B_maior=8.0;
+		h=5.0;
+		
+		area = ((b_menor + B_maior) / 2) *h;
+		
+		System.out.println(area);
+		*/
+		
+		int a,b;
+		double resultado;
+		
+		a = 5;
+		b= 2;
+		
+		resultado =(double) a/b; // Fazendo casting (conversão explicita de "int" para "double")
+		
+		System.out.println(resultado);
+		
+		
+		double x;
+		int y;
+		
+		x = 5.0;
+		y = (int) x; //Fazendo casting
+		
+		System.out.println(y);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
